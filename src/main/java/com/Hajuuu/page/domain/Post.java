3cbb6551ede70ecd65f854bacd399da3df47215c
Hajuuu <hpt0127@naver.com>
2024-01-19 20:20:11 +0900

    refactor(Post): 추상 클래스로 변경

--- src/main/java/com/Hajuuu/page/domain/Post.java
+++ src/main/java/com/Hajuuu/page/domain/Post.java
@@ -2,19 +2,25 @@ package com.Hajuuu.page.domain;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Getter
-public class Post {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Post {
 
     @Id @GeneratedValue
     @Column(name = "post_id")
@@ -25,14 +31,6 @@ public class Post {
     private User user;
 
     private String content;
-
     private LocalDateTime date;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    @JoinColumn(name = "book_id")
-    private Book book;
-
-    public void setBook(Book book) {
-        this.book = book;
-    }
 }
