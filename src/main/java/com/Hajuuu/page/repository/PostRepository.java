1bde7bd11607c23c9e40afb8e286141c300073f7
Hajuuu <hpt0127@naver.com>
2024-01-19 20:25:53 +0900

    feat(PostRepository): PostRepository 작성

--- src/main/java/com/Hajuuu/page/repository/PostRepository.java
+++ src/main/java/com/Hajuuu/page/repository/PostRepository.java
@@ -1,7 +1,30 @@
 package com.Hajuuu.page.repository;
 
+import com.Hajuuu.page.domain.Post;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class PostRepository {
+
+    private final EntityManager em;
+
+    public void save(Post post) {
+        if(post.getId() == null) {
+            em.persist(post);
+        } else {
+            em.merge(post);
+        }
+    }
+
+    public Post findPost(Long id) {
+        return em.find(Post.class, id);
+    }
+
+    public List<Post> findPost() {
+        return em.createQuery("select i from Post i", Post.class).getResultList();
+    }
 }
