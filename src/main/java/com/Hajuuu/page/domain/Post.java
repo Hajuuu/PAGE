0ba9ecb93b7cdc1b42c148fdab57243d22792a17
Hajuuu <hpt0127@naver.com>
2024-01-22 22:41:51 +0900

    refactor(BookRepository): createPost 메서드 작성 및 연관관계 변경(다대일)

--- src/main/java/com/Hajuuu/page/domain/Post.java
+++ src/main/java/com/Hajuuu/page/domain/Post.java
@@ -13,14 +13,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
 @Getter @Setter
-public abstract class Post {
+public class Post {
 
     @Id @GeneratedValue
     @Column(name = "post_id")
@@ -30,7 +29,20 @@ public abstract class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
     private String content;
     private LocalDateTime date;
+
+    public static Post createPost(User user, Book book) {
+        Post post = new Post();
+        post.setBook(book);
+        post.setContent("가나다라마바사차");
+        post.setUser(user);
+        return post;
+    }
+
 
 }
