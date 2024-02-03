42f701c4786a30fa5497332f158902ad7fb3fb9d
Hajuuu <hpt0127@naver.com>
2024-02-03 17:48:47 +0900

    feat(Post): 연관관계 메서드 작성

--- src/main/java/com/Hajuuu/page/domain/Post.java
+++ src/main/java/com/Hajuuu/page/domain/Post.java
@@ -1,27 +1,21 @@
 package com.Hajuuu.page.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
 public class Post {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "post_id")
     private Long id;
 
@@ -36,12 +30,21 @@ public class Post {
     private String content;
     private LocalDateTime date;
 
-    public static Post createPost(User user, Book book) {
-        Post post = new Post();
-        post.setBook(book);
-        post.setContent("가나다라마바사차");
-        post.setUser(user);
-        return post;
+    public void createPost(User user, Book book, String content) {
+        setUser(user);
+        setBook(book);
+        this.content = content;
+    }
+
+    //==연관관계 메서드==//
+    public void setUser(User user) {
+        this.user = user;
+        user.getPosts().add(this);
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+        book.getPosts().add(this);
     }
 
 
