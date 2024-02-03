203ee46e982f8dc1b6b7ae63bf24414975fc9d4a
Hajuuu <hpt0127@naver.com>
2024-02-03 17:46:59 +0900

    feat(UserRepository): findAll, findByName 메서드 작성

--- src/main/java/com/Hajuuu/page/repository/UserRepository.java
+++ src/main/java/com/Hajuuu/page/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.Hajuuu.page.repository;
 import com.Hajuuu.page.domain.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,7 +17,17 @@ public class UserRepository {
         return user.getId();
     }
 
-    public User find(Long id) {
+    public User findOne(Long id) {
         return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    public List<User> findByName(String name) {
+        return em.createQuery("select u from User u where u.name = :name", User.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
