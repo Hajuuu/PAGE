df2c152000dfa5d25eeb4f44cc0dab809fedc008
Hajuuu <hpt0127@naver.com>
2024-02-03 17:47:51 +0900

    feat(User): createUser 메서드 작성

--- src/main/java/com/Hajuuu/page/domain/User.java
+++ src/main/java/com/Hajuuu/page/domain/User.java
@@ -1,5 +1,6 @@
 package com.Hajuuu.page.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,20 +9,26 @@ import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
 public class User {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "user_id")
     private Long id;
     private String passWord;
     private String name;
     private String email;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Post> posts = new ArrayList<>();
+
+    public void createUser(String passWord, String name, String email) {
+        this.passWord = passWord;
+        this.name = name;
+        this.email = email;
+    }
 
 }
