ecfd196037192fe6eb94dcaab088efb8b9a19b5d
Hajuuu <hpt0127@naver.com>
2024-02-14 15:05:46 +0900

    refactor(AladinDetailInfo): RequiredArgsConstructor 어노테이션 추가

--- src/main/java/com/Hajuuu/page/api/AladinDetailInfo.java
+++ src/main/java/com/Hajuuu/page/api/AladinDetailInfo.java
@@ -1,18 +1,14 @@
 package com.Hajuuu.page.api;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Data
+@RequiredArgsConstructor
 public class AladinDetailInfo {
 
     private String subTitle;
     private String originalTitle;
     private String itemPage;
 
-    AladinDetailInfo() {
-
-    }
 }
