94f256f24ed4ab876924d2d750b69668614f0d31
Hajuuu <hpt0127@naver.com>
2024-03-07 10:43:30 +0900

    feat(LoginService): LoginService 작성

---
+++ src/main/java/com/Hajuuu/page/service/LoginService.java
@@ -0,0 +1,19 @@
+package com.Hajuuu.page.service;
+
+import com.Hajuuu.page.domain.User;
+import com.Hajuuu.page.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    public User login(String loginId, String password) {
+        return userRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
