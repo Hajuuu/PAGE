56b3667199da6ccc82a9cb5a83287f32d1cce49b
Hajuuu <hpt0127@naver.com>
2024-03-11 16:10:33 +0900

    feat(LoginService): Transactional 작성

--- src/main/java/com/Hajuuu/page/service/LoginService.java
+++ src/main/java/com/Hajuuu/page/service/LoginService.java
@@ -4,8 +4,10 @@ import com.Hajuuu.page.domain.User;
 import com.Hajuuu.page.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LoginService {
 
