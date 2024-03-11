ed8827624755b43a4ad81fc6cfff8185e9c022f7
Hajuuu <hpt0127@naver.com>
2024-03-11 16:21:12 +0900

    feat(UserService): UserService 작성

---
+++ src/main/java/com/Hajuuu/page/service/UserService.java
@@ -0,0 +1,46 @@
+package com.Hajuuu.page.service;
+
+import com.Hajuuu.page.domain.Book;
+import com.Hajuuu.page.domain.User;
+import com.Hajuuu.page.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long join(User user) {
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        Optional<User> findUsers = userRepository.findByLoginId(user.getLoginId());
+        if (findUsers != null) {
+            throw new IllegalStateException("사용할 수 없는 이름입니다.");
+        }
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(Long userId) {
+        return userRepository.findOne(userId);
+    }
+
+    public List<Book> findBooks(String loginId) {
+        return userRepository.findBooks(loginId);
+    }
+
+}
