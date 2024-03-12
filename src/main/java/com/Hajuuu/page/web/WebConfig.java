d21748cf200564491a9eb46652f0d945fba6378b
Hajuuu <hpt0127@naver.com>
2024-03-12 10:00:00 +0900

    feat(WebConfig): ArgumentResolver 적용

--- src/main/java/com/Hajuuu/page/web/WebConfig.java
+++ src/main/java/com/Hajuuu/page/web/WebConfig.java
@@ -1,39 +1,36 @@
 package com.Hajuuu.page.web;
 
 
-import com.Hajuuu.page.web.filter.LogFilter;
-import com.Hajuuu.page.web.filter.LoginCheckFilter;
-import jakarta.servlet.Filter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
+import com.Hajuuu.page.web.argumentresolver.LoginMemberArgumentResolver;
+import com.Hajuuu.page.web.interceptor.LogInterceptor;
+import com.Hajuuu.page.web.interceptor.LoginCheckInterceptor;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LogInterceptor())
-//                .order(1)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/pagecss/**", "/*.ico", "/error");
-//    }
-
-    //@Bean
-    public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LogFilter());
-        filterRegistrationBean.setOrder(1);
-        filterRegistrationBean.addUrlPatterns("/*");
-        return filterRegistrationBean;
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
     }
 
-    @Bean
-    public FilterRegistrationBean loginCheckFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LoginCheckFilter());
-        filterRegistrationBean.setOrder(2);
-        filterRegistrationBean.addUrlPatterns("/*");
-        return filterRegistrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "/assets/**", "/js/*",
+                        "/style.css", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/logout", "/join", "/css/*", "/assets/**", "/js/*",
+                        "/style.css", "/*.ico", "/error");
     }
+
 }
