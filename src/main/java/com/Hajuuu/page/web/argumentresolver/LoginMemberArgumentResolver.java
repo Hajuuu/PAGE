95b0b5463bddb6857c52a9e22109f2e4b64a3120
Hajuuu <hpt0127@naver.com>
2024-03-12 09:59:13 +0900

    feat(LoginMemberArgumentResolver): ArgumentResolver 구현

---
+++ src/main/java/com/Hajuuu/page/web/argumentresolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,36 @@
+package com.Hajuuu.page.web.argumentresolver;
+
+import com.Hajuuu.page.controller.SessionConst;
+import com.Hajuuu.page.domain.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        log.info("supportsParameter 실행");
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasUserType = User.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasUserType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info("resolveArgument 실행");
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return null;
+        }
+
+        return session.getAttribute(SessionConst.LOGIN_USER);
+    }
+}
