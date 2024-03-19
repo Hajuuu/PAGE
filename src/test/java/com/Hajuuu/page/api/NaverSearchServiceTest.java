9c0ae207b2bcec939bb93d627c1f5185c7b53e8e
Hajuuu <hpt0127@naver.com>
2024-03-19 18:37:02 +0900

    test(NaverSearchServiceTest): naver api 연결 확인 테스트 작성

--- src/test/java/com/Hajuuu/page/api/NaverSearchServiceTest.java
+++ src/test/java/com/Hajuuu/page/api/NaverSearchServiceTest.java
@@ -1,7 +1,5 @@
 package com.Hajuuu.page.api;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,12 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NaverSearchServiceTest {
 
-    @Autowired NaverSearchService naverSearchService;
+    @Autowired
+    NaverSearchService naverSearchService;
 
     @Test
     void checkConnect() {
         String title = "물고기는 존재하지 않는다.";
-        naverSearchService.searchBook(title);
+        naverSearchService.getBookInfo(title);
     }
 
 }
