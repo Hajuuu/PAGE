922979f83fc66cc79ed5d63d24f5d8e5e7fd9520
Hajuuu <hpt0127@naver.com>
2024-04-21 21:13:58 +0900

    refactor(UserRepository): interface로 변경 및 Spring Data JPA 적용

--- src/main/java/com/Hajuuu/page/repository/UserRepository.java
+++ src/main/java/com/Hajuuu/page/repository/UserRepository.java
@@ -1,39 +1,12 @@
 package com.Hajuuu.page.repository;
 
-import com.Hajuuu.page.domain.Book;
 import com.Hajuuu.page.domain.User;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public User save(User user) {
-        em.persist(user);
-        return user;
-    }
-
-    public User findOne(Long id) {
-        return em.find(User.class, id);
-    }
-
-    public Optional<User> findByLoginId(String loginId) {
-        return findAll().stream()
-                .filter(u -> u.getLoginId().equals(loginId)).findFirst();
-    }
-
-    public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class)
-                .getResultList();
-    }
-
-    public List<Book> findBooks(String loginId) {
-        return findByLoginId(loginId).get().getBooks();
-    }
+    Optional<User> findByLoginId(String loginId);
 }
