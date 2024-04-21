c526c20ab6f5a1765e4fafb488ec7937f2e146b2
Hajuuu <hpt0127@naver.com>
2024-04-21 21:16:31 +0900

    refactor(UserService): validateDuplicateUser 메서드 변경 및 Spring Data JPA 적용

--- src/main/java/com/Hajuuu/page/service/UserService.java
+++ src/main/java/com/Hajuuu/page/service/UserService.java
@@ -20,27 +20,33 @@ public class UserService {
 
     @Transactional
     public Long join(User user) {
+        validateDuplicateUser(user.getLoginId());
         userRepository.save(user);
         return user.getId();
     }
 
-    private void validateDuplicateUser(User user) {
-        Optional<User> findUsers = userRepository.findByLoginId(user.getLoginId());
-        if (findUsers != null) {
+    private void validateDuplicateUser(String loginId) {
+        Optional<User> findUsers = userRepository.findByLoginId(loginId);
+        if (!findUsers.isEmpty()) {
             throw new IllegalStateException("사용할 수 없는 이름입니다.");
         }
+
+    }
+
+    public Optional<User> findByLoginId(String loginId) {
+        return userRepository.findByLoginId(loginId);
     }
 
     public List<User> findUsers() {
         return userRepository.findAll();
     }
 
-    public User findOne(Long userId) {
-        return userRepository.findOne(userId);
+    public Optional<User> findOne(Long userId) {
+        return userRepository.findById(userId);
     }
 
     public List<Book> findBooks(String loginId) {
-        return userRepository.findBooks(loginId);
+        return findByLoginId(loginId).get().getBooks();
     }
 
 }
