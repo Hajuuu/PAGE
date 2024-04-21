3bb1deff89ababb6fc9b21409ff1763541e574a0
Hajuuu <hpt0127@naver.com>
2024-04-21 21:17:43 +0900

    refactor(LoginService): Spring Data JPA 적용

--- src/main/java/com/Hajuuu/page/service/LoginService.java
+++ src/main/java/com/Hajuuu/page/service/LoginService.java
@@ -2,6 +2,7 @@ package com.Hajuuu.page.service;
 
 import com.Hajuuu.page.domain.User;
 import com.Hajuuu.page.repository.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +14,8 @@ public class LoginService {
 
     private final UserRepository userRepository;
 
-    public User login(String loginId, String password) {
+    public Optional<User> login(String loginId, String password) {
         return userRepository.findByLoginId(loginId)
-                .filter(m -> m.getPassword().equals(password))
-                .orElse(null);
+                .filter(m -> m.getPassword().equals(password));
     }
 }
