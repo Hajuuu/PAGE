b42d192f3fd91979818886e6b3d4c7d93331329d
Hajuuu <hpt0127@naver.com>
2024-04-22 17:00:26 +0900

    feat(PostRepositoryImpl): PostRepositoryImpl 작성

---
+++ src/main/java/com/Hajuuu/page/repository/PostRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.Hajuuu.page.repository;
+
+import static com.Hajuuu.page.domain.QPost.post;
+import static org.hibernate.internal.util.StringHelper.isEmpty;
+
+import com.Hajuuu.page.DTO.PostFormDTO;
+import com.Hajuuu.page.DTO.QPostFormDTO;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+public class PostRepositoryImpl implements PostRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public PostRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<PostFormDTO> search(Long bookId) {
+        return queryFactory
+                .select(new QPostFormDTO(
+                        post.content,
+                        post.createdTime,
+                        post.book.id,
+                        post.book.page))
+                .from(post)
+                .where(bookIdEq(bookId))
+                .fetch();
+    }
+
+    private BooleanExpression bookIdEq(Long bookId) {
+        return isEmpty(String.valueOf(bookId)) ? null : post.book.id.eq(bookId);
+    }
+
+}
