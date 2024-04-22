9ae9c806587426b5107854b70314c47980d678e1
Hajuuu <hpt0127@naver.com>
2024-04-22 17:08:35 +0900

    refactor(PostRepository): Spring Data JPA 적용

--- src/main/java/com/Hajuuu/page/repository/PostRepository.java
+++ src/main/java/com/Hajuuu/page/repository/PostRepository.java
@@ -1,34 +1,10 @@
 package com.Hajuuu.page.repository;
 
 import com.Hajuuu.page.domain.Post;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class PostRepository {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public void save(Post post) {
-        em.persist(post);
-    }
-
-    public Post findOne(Long id) {
-        return em.find(Post.class, id);
-    }
-
-    public List<Post> findAll() {
-        return em.createQuery("select i from Post i", Post.class).getResultList();
-    }
-
-    public List<Post> findAllById(Long bookId) {
-        return em.createQuery("select i from Post i where i.book.id = :bookId", Post.class)
-                .setParameter("bookId", bookId)
-                .getResultList();
-    }
 }
