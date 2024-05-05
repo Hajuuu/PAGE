8a0cccfb876d7b24042af7df27081c4543348003
Hajuuu <hpt0127@naver.com>
2024-05-05 17:42:37 +0900

    feat(BookService): findOne 메서드 구현 및 id int형으로 변경

--- src/main/java/com/Hajuuu/page/service/BookService.java
+++ src/main/java/com/Hajuuu/page/service/BookService.java
@@ -1,12 +1,13 @@
 package com.Hajuuu.page.service;
 
-import com.Hajuuu.page.api.SearchBookDTO;
+import com.Hajuuu.page.DTO.SearchBookDTO;
 import com.Hajuuu.page.controller.SessionConst;
 import com.Hajuuu.page.domain.Book;
 import com.Hajuuu.page.domain.User;
 import com.Hajuuu.page.repository.BookRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.SessionAttribute;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookService {
+    @Autowired
     private final BookRepository bookRepository;
 
     @Transactional
@@ -28,11 +30,15 @@ public class BookService {
         return book.getId();
     }
 
-    private void validateDuplicateBook(Long userId, String isbn) {
+    private void validateDuplicateBook(int userId, String isbn) {
         List<Book> findBooks = bookRepository.findAllById(userId);
         if (findBooks.stream().filter(b -> b.getIsbn().equals(isbn)).count() > 0) {
             throw new IllegalStateException("이미 저장한 책이에요!");
         }
+    }
+
+    public Book findOne(int id) {
+        return bookRepository.findOne(id);
     }
 
     public List<Book> findAll() {
