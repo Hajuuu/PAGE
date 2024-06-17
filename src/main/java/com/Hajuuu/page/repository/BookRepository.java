7581a381cf2f8a4d7dce9d762fced7e355d780cd
Hajuuu <hpt0127@naver.com>
2024-06-17 22:40:07 +0900

    refactor(BookRepository): Spring Data JPA 적용

--- src/main/java/com/Hajuuu/page/repository/BookRepository.java
+++ src/main/java/com/Hajuuu/page/repository/BookRepository.java
@@ -1,40 +1,11 @@
 package com.Hajuuu.page.repository;
 
 import com.Hajuuu.page.domain.Book;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RequiredArgsConstructor
-public class BookRepository {
+public interface BookRepository extends JpaRepository<Book, Integer>, BookRepositoryCustom {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    public void save(Book book) {
-        em.persist(book);
-    }
-
-    public Book findOne(int id) {
-        return em.find(Book.class, id);
-    }
-
-    public List<Book> findAll() {
-        return em.createQuery("select i from Book i", Book.class).getResultList();
-    }
-
-    public List<Book> findAllById(int userId) {
-        return em.createQuery("select i from Book i where i.user.id = :userId", Book.class)
-                .setParameter("userId", userId)
-                .getResultList();
-    }
-
-    public List<Book> findAllByTitle(String title) {
-        return em.createQuery("select i from Book i where i.title = :title", Book.class)
-                .setParameter("title", title)
-                .getResultList();
-    }
 }
+
