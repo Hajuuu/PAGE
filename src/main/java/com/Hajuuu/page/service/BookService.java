4ec910afd04a0bec057739825e7dcebab0b86e29
Hajuuu <hpt0127@naver.com>
2024-06-27 23:47:26 +0900

    refactor(BookService): Book -> SearchBookDTO 변경

--- src/main/java/com/Hajuuu/page/service/BookService.java
+++ src/main/java/com/Hajuuu/page/service/BookService.java
@@ -1,26 +1,26 @@
 package com.Hajuuu.page.service;
 
 import com.Hajuuu.page.DTO.SearchBookDTO;
-import com.Hajuuu.page.controller.SessionConst;
 import com.Hajuuu.page.domain.Book;
 import com.Hajuuu.page.domain.User;
 import com.Hajuuu.page.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookService {
+
     @Autowired
     private final BookRepository bookRepository;
 
     @Transactional
-    public long saveBook(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser,
+    public long saveBook(User loginUser,
                          SearchBookDTO searchBookDTO) {
         Book book = new Book();
         book.createBook(loginUser, searchBookDTO.getTitle(), searchBookDTO.getAuthor(), searchBookDTO.getPage(),
@@ -31,22 +31,22 @@ public class BookService {
     }
 
     private void validateDuplicateBook(int userId, String isbn) {
-        List<Book> findBooks = bookRepository.findAllById(userId);
+        List<SearchBookDTO> findBooks = bookRepository.search(userId);
         if (findBooks.stream().filter(b -> b.getIsbn().equals(isbn)).count() > 0) {
             throw new IllegalStateException("이미 저장한 책이에요!");
         }
     }
 
-    public Book findOne(int id) {
-        return bookRepository.findOne(id);
+    public Optional<Book> findById(int id) {
+        return bookRepository.findById(id);
     }
 
     public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
-    public List<Book> findBooks(User user) {
-        return bookRepository.findAllById(user.getId());
+    public List<SearchBookDTO> search(int userId) {
+        return bookRepository.search(userId);
     }
 
 
