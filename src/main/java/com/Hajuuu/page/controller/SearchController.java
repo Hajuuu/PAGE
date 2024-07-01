e60a5775ab3795f29b76e818116832891ca1ee6a
Hajuuu <hpt0127@naver.com>
2024-07-01 22:13:07 +0900

    feat(SearchController): titleInvalid, searchForm 메서드 추가

--- src/main/java/com/Hajuuu/page/controller/SearchController.java
+++ src/main/java/com/Hajuuu/page/controller/SearchController.java
@@ -1,17 +1,23 @@
 package com.Hajuuu.page.controller;
 
+import com.Hajuuu.page.DTO.SearchUserDTO;
+import com.Hajuuu.page.DTO.UserDTO;
 import com.Hajuuu.page.api.NaverBookDTO;
 import com.Hajuuu.page.api.NaverBookInfo;
 import com.Hajuuu.page.api.NaverSearchService;
-import java.io.IOException;
+import com.Hajuuu.page.domain.User;
+import com.Hajuuu.page.service.UserService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -19,20 +25,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class SearchController {
 
     private final NaverSearchService naverSearchService;
+    private final UserService userService;
 
     @GetMapping("/search")
-    public String searchTitle(Model model)
-            throws IOException {
+    public String searchTitle(User loginUser, Model model) {
         model.addAttribute(new NaverBookInfo());
         return "/search/naverBook";
     }
 
     @PostMapping("/search")
-    public String searchBook(@ModelAttribute("title") String title,
-                             @ModelAttribute("items") NaverBookInfo naverBookInfo,
-                             Model model)
-            throws IOException {
-        if (title == null || title.isBlank() || title.isEmpty()) {
+    public String searchBook(User loginUser, @ModelAttribute("title") String title,
+                             Model model) {
+        if (titleInvalid(title)) {
             return "/search/naverBook";
         }
         NaverBookDTO bookInfo = naverSearchService.getBookInfo(title);
@@ -43,5 +47,52 @@ public class SearchController {
         return "/search/naverBook";
     }
 
+    private boolean titleInvalid(String title) {
+        return title == null || title.isBlank() || title.isEmpty();
+    }
 
+    @GetMapping("/search/user")
+    public String searchForm(User loginUser, Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "/search/follow";
+    }
+
+    @PostMapping("/search/user")
+    public String searchUser(User loginUser, @ModelAttribute("loginId") String loginId,
+                             Model model) {
+
+        if (titleInvalid(loginId)) {
+            return "/search/follow";
+        }
+        List<Integer> followingList = loginUser.getFollowingList();
+        List<SearchUserDTO> users = userService.search(loginId);
+        for (SearchUserDTO user : users) {
+            if (followingList.contains(user.getId())) {
+                user.setCheck(true);
+            }
+
+        }
+
+        model.addAttribute("loginId", loginId);
+        model.addAttribute("users", users);
+        return "/search/follow";
+    }
+
+    @PostMapping("/search/follow/{userId}")
+    public String follow(User loginUser, @PathVariable("userId") int id,
+                         RedirectAttributes redirectAttributes) {
+
+        userService.addFollowing(loginUser, id);
+        List<Integer> followingList = loginUser.getFollowingList();
+        redirectAttributes.addFlashAttribute("users", followingList);
+        return "redirect:/mypage";
+    }
+
+    @PostMapping("/search/cancel/{userId}")
+    public String cancel(User loginUser, @PathVariable("userId") int id, Model model) {
+        loginUser.cancelFollowing(id);
+        Optional<User> user = userService.findOne(id);
+        user.get().cancelFollower(loginUser.getId());
+        return "cancel";
+    }
 }
