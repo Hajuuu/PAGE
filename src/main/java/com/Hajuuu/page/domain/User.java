72fe90309805f4291aa604d95863c4c8fbe766e4
Hajuuu <hpt0127@naver.com>
2024-07-02 23:53:46 +0900

    feat(User): Builder 추가

--- src/main/java/com/Hajuuu/page/domain/User.java
+++ src/main/java/com/Hajuuu/page/domain/User.java
@@ -4,28 +4,40 @@ import com.Hajuuu.page.DTO.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.OneToOne;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
     @Column(name = "user_id")
     private int id;
-    @NotEmpty
     private String loginId;
-    @NotEmpty
     private String password;
+
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String provider;
+    private String providerId;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Book> books = new ArrayList<>();
@@ -33,12 +45,15 @@ public class User extends BaseTimeEntity {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Post> posts = new ArrayList<>();
 
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private NaverUser naverUser;
+
     private List<Integer> followingList = new ArrayList<>();
     private List<Integer> followerList = new ArrayList<>();
 
-    public void createUser(String loginId, String password) {
-        this.loginId = loginId;
-        this.password = password;
+    public User updateName(String name) {
+        this.name = name;
+        return this;
     }
 
     public void addFollowing(int id) {
@@ -57,5 +72,8 @@ public class User extends BaseTimeEntity {
         this.followerList.remove(id);
     }
 
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 
 }
