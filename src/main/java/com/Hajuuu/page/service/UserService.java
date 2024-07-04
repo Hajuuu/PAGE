eb2b14ff224179b52cd27cebd4ff86beb9557c4a
Hajuuu <hpt0127@naver.com>
2024-07-04 21:06:52 +0900

    feat(UserService): BCryptPasswordEncoder 추가

--- src/main/java/com/Hajuuu/page/service/UserService.java
+++ src/main/java/com/Hajuuu/page/service/UserService.java
@@ -2,6 +2,7 @@ package com.Hajuuu.page.service;
 
 import com.Hajuuu.page.DTO.PostDTO;
 import com.Hajuuu.page.DTO.SearchUserDTO;
+import com.Hajuuu.page.DTO.UserDTO;
 import com.Hajuuu.page.domain.Book;
 import com.Hajuuu.page.domain.User;
 import com.Hajuuu.page.repository.UserRepository;
@@ -9,8 +10,8 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +20,19 @@ public class UserService {
     @Autowired
     private final UserRepository userRepository;
 
-    @Transactional
-    public int join(User user) {
-        validateDuplicateUser(user.getLoginId());
-        userRepository.save(user);
-        return user.getId();
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void securityJoin(UserDTO userDTO) {
+        if (userRepository.existsByLoginId(userDTO.getLoginId())) {
+            return;
+        }
+        userDTO.setPassword(bCryptPasswordEncoder.encode(userDTO.getPassword()));
+        userRepository.save(userDTO.toEntity());
     }
 
-    private void validateDuplicateUser(String loginId) {
-        Optional<User> findUsers = userRepository.findByLoginId(loginId);
+    private void validateDuplicateUser(String email) {
+        Optional<User> findUsers = userRepository.findByEmail(email);
         if (!findUsers.isEmpty()) {
             throw new IllegalStateException("사용할 수 없는 이름입니다.");
         }
@@ -47,7 +52,7 @@ public class UserService {
     }
 
     public List<Book> findBooks(String loginId) {
-        return userRepository.findByLoginId(loginId).get().getBooks();
+        return userRepository.findByLoginId(loginId).getBooks();
     }
 
     public void addFollowing(User user, int id) {
@@ -57,8 +62,22 @@ public class UserService {
     }
 
     public List<PostDTO> findFollowingUsersPost(User user) {
+        if (user == null) {
+            return null;
+        }
         List<Integer> followingList = user.getFollowingList();
+
         List<PostDTO> following = userRepository.following(followingList);
         return following;
+    }
+
+    public User findByEmail(String email) {
+        Optional<User> findUser = userRepository.findByEmail(email);
+        return findUser.orElse(null);
+    }
+
+    public User findByLoginId(String loginId) {
+        User findUser = userRepository.findByLoginId(loginId);
+        return findUser;
     }
 }
