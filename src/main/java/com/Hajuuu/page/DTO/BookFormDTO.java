74e953238724021c3829c62ce47012396e55d636
Hajuuu <hpt0127@naver.com>
2024-08-22 20:05:59 +0900

    feat(BookFormDTO): 책 등록 DTO 구현

---
+++ src/main/java/com/Hajuuu/page/DTO/BookFormDTO.java
@@ -0,0 +1,47 @@
+package com.Hajuuu.page.DTO;
+
+import com.Hajuuu.page.api.BookState;
+import com.querydsl.core.annotations.QueryProjection;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BookFormDTO {
+
+    @NotBlank
+    private String title;
+
+    private String image;
+
+    @NotNull
+    @Min(1)
+    private Long page;
+
+    @NotBlank
+    private String author;
+
+    @NotBlank
+    private String isbn;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private BookState bookState;
+
+    @QueryProjection
+    public BookFormDTO(String title, String image, Long page, String author, String isbn,
+                       BookState bookState) {
+        this.title = title;
+        this.image = image;
+        this.page = page;
+        this.author = author;
+        this.isbn = isbn;
+        this.bookState = bookState;
+    }
+
+}
