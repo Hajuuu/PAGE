e32d6e343671f5f839c7f7f92f00c64d60635c8b
Hajuuu <hpt0127@naver.com>
2024-09-02 23:54:45 +0900

    feat: 파일 저장 기능 추가

---
+++ src/main/java/com/Hajuuu/page/service/FileStore.java
@@ -0,0 +1,55 @@
+package com.Hajuuu.page.service;
+
+
+import com.Hajuuu.page.domain.UploadFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileStore {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    public String getFullPath(String fileName) {
+        return fileDir + fileName;
+    }
+
+    public List<UploadFile> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<UploadFile> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    public UploadFile storeFile(MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String originalName = multipartFile.getOriginalFilename();
+        String storeName = createStoreFileName(originalName);
+        multipartFile.transferTo(new File(getFullPath(storeName)));
+        return new UploadFile(originalName, storeName);
+    }
+
+    private String createStoreFileName(String originalName) {
+        String extension = extractExtension(originalName);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + extension;
+    }
+
+    private String extractExtension(String originalName) {
+        int pos = originalName.lastIndexOf(".");
+        return originalName.substring(pos + 1);
+    }
+}
