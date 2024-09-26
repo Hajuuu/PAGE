132f00047ff4498aa4e17c8d01f445e72d627efc
Hajuuu <hpt0127@naver.com>
2024-09-26 23:46:39 +0900

    feat: 프로필 이미지 변경 기능 구현

--- src/main/java/com/Hajuuu/page/controller/HomeController.java
+++ src/main/java/com/Hajuuu/page/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.Hajuuu.page.controller;
 
 import com.Hajuuu.page.DTO.FollowDTO;
+import com.Hajuuu.page.DTO.SettingDTO;
 import com.Hajuuu.page.DTO.UserDTO;
 import com.Hajuuu.page.domain.LoginForm;
+import com.Hajuuu.page.domain.UploadFile;
 import com.Hajuuu.page.domain.User;
+import com.Hajuuu.page.service.FileStore;
 import com.Hajuuu.page.service.LoginService;
 import com.Hajuuu.page.service.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +23,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -26,6 +31,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -34,6 +40,7 @@ public class HomeController {
 
     private final LoginService loginService;
     private final UserService userService;
+    private final FileStore fileStore;
 
     @GetMapping("/")
     public String homeLogin(Model model) {
@@ -111,9 +118,37 @@ public class HomeController {
         String loginId = authentication.getName();
 
         User findUser = userService.findByLoginId(loginId);
-        model.addAttribute("user", findUser);
+        SettingDTO settingDTO = new SettingDTO();
+        settingDTO.setLoginId(findUser.getLoginId());
+        settingDTO.setEmail(findUser.getEmail());
+        settingDTO.setPassword(findUser.getPassword());
+        settingDTO.setImage(findUser.getImage());
+        model.addAttribute("user", settingDTO);
         return "my/setting";
     }
+
+    @PostMapping("/setting/fileupload")
+    @Transactional
+    public String fileUpload(@ModelAttribute SettingDTO settingDTO, RedirectAttributes redirectAttributes)
+            throws IOException {
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String loginId = authentication.getName();
+
+        UploadFile uploadFile = fileStore.storeFile(settingDTO.getImageFile());
+
+        String filename_url = uploadFile.getUploadName();
+        String message = uploadFile.getUploadName() + " 파일이 저장되었습니다.";
+
+        User findUser = userService.findByLoginId(loginId);
+        findUser.updateProfile(fileStore.getFullPath(uploadFile.getUploadName()));
+        redirectAttributes.addFlashAttribute("message", message);
+        redirectAttributes.addFlashAttribute("filename_url", filename_url);
+
+        return "redirect:/";
+    }
+
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
