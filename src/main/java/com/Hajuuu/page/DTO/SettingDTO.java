73d045b7872cef69b91c9a6d1a89023188119a00
Hajuuu <hpt0127@naver.com>
2024-09-30 23:23:54 +0900

    feat: DTO 추가

---
+++ src/main/java/com/Hajuuu/page/DTO/SettingDTO.java
@@ -0,0 +1,16 @@
+package com.Hajuuu.page.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@NoArgsConstructor
+public class SettingDTO {
+
+    private String loginId;
+    private String password;
+    private MultipartFile imageFile;
+    private String image;
+    private String email;
+}
