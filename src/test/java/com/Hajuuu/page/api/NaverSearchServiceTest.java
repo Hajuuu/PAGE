e70285bab5eaa9239415eb888ffadc2aa7633ab7
Hajuuu <hpt0127@naver.com>
2024-10-10 23:26:45 +0900

    test: 네이버 api 테스트 작성

--- src/test/java/com/Hajuuu/page/api/NaverSearchServiceTest.java
+++ src/test/java/com/Hajuuu/page/api/NaverSearchServiceTest.java
@@ -1,19 +1,57 @@
 package com.Hajuuu.page.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+import com.Hajuuu.page.controller.SearchController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
 class NaverSearchServiceTest {
 
-    @Autowired
-    NaverSearchService naverSearchService;
+    @InjectMocks
+    private SearchController searchController;
+    @Mock
+    private NaverSearchService naverSearchService;
 
+    @Mock
+    private NaverBookDTO bookInfo;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(naverSearchService).build();
+    }
+
+    @DisplayName("api 연결 성공")
     @Test
-    void checkConnect() {
-        String title = "물고기는 존재하지 않는다.";
-        naverSearchService.getBookInfo(title);
+    void checkConnect() throws Exception {
+        //given
+        String title = "물고기는 존재하지 않는다";
+
+        doReturn(bookInfo).when(naverSearchService).getBookInfo(title);
+
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(title)
+        );
+
+        resultActions.andExpect((ResultMatcher) jsonPath(bookInfo.getItems().get(0).getTitle()))
+                .equals("물고기는 존재하지 않는다");
     }
 
 }
